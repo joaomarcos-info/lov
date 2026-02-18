1e6c49d09fa5e5464b3437582965ecbb9b0113a8
joaomarcos-info <rosal.joao@outlook.com>
2026-02-18 16:50:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,10 +52,10 @@ João.`;
         // GALERIA DO CASAL (galeriaDoCasal)
     // Dica: Substitua as URLs pelas fotos reais do casal
     const galeriaDoCasal = [
-        { url: "01.jpeg", legenda: "Nosso primeiro jantar romântico" },
-        { url: "02.jpeg", legenda: "Passeio inesquecível no parque" },
-        { url: "03.jpeg", legenda: "Celebrando pequenas vitórias" },
-        { url: "04.jpeg", legenda: "Sempre juntos, em qualquer lugar" }
+        { url: "/01.jpeg", legenda: "Nosso primeiro jantar romântico" },
+        { url: "/02.jpeg", legenda: "Passeio inesquecível no parque" },
+        { url: "/03.jpeg", legenda: "Celebrando pequenas vitórias" },
+        { url: "/04.jpeg", legenda: "Sempre juntos, em qualquer lugar" }
     ];
 
     // ==========================================
